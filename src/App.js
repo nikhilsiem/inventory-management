eee41ebe79946bfb546793ac2a4c8e2d411089be
Nikhil Singh <nikhils.iem@gmail.com>
2025-01-22 23:30:10 +0530

    Initial React app commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AdminView from './views/AdminView';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Inventory Management System</h1>
+      <AdminView />
+    </div>
+  );
+}
+
+export default App;
